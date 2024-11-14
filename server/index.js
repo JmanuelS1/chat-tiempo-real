6eda2b4b6a4a07dd878b0b6281248e0aab870089
Jesús Sánchez <jmsr060391@gmail.com>
2024-11-14 14:09:10 -0500

    ver en 9antalla chat

--- server/index.js
+++ server/index.js
@@ -1,24 +1,44 @@
-import express from 'express'
-import logger from 'morgan'
-import { Server } from 'socket.io'
-import { createServer } from 'node:http'
+import express from 'express';
+import logger from 'morgan';
+import { Server } from 'socket.io';
+import { createServer } from 'node:http';
 
-const port = process.env.PORT ?? 3000
+const port = process.env.PORT ?? 3000;
 
-const app = express()
-const server = createServer(app)
-const io = new Server(server)
+const app = express();
+const server = createServer(app);
 
-io.on('connection', () => {
-   console.log('a new client connected')
-})
+const io = new Server(server, {
+   cors: {
+      origin: "*", 
+      methods: ["GET", "POST"],
+      allowedHeaders: ["Content-Type"],
+   },
+});
 
-app.use(logger('dev'))
+io.on('connection', (socket) => {
+   console.log('A new client connected');
+
+   socket.on('message', (data) => {
+      console.log('Mensaje recibido:', data); 
+      io.emit('message', data);
+   });
+
+   socket.on('disconnect', () => {
+      console.log('A client disconnected');
+   });
+
+   socket.on('chat message', (msg) => {
+      io.emit('chat message', msg);
+   })
+});
+
+app.use(logger('dev'));
 
 app.get('/', (req, res) => {
-   res.sendFile(process.cwd() + '/client/index.html')
-})
+   res.sendFile(process.cwd() + '/client/index.html');
+});
 
-app.listen(port, () => {
-   console.log(`Servidor escuchando en el puerto ${port}`)
-})
+server.listen(port, () => {
+   console.log('Servidor escuchando en el puerto port');
+});
